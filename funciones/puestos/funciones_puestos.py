6eb01bf450ac5338a8164afabbaf28d056067c93
Abraham-Torres <abraham_torres81@outlook.com>
2022-11-03 17:14:49 -0600

    dropdown en editar informacion funcionando

--- funciones/puestos/funciones_puestos.py
+++ funciones/puestos/funciones_puestos.py
@@ -1,16 +1,19 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
 from data_base import database as mongodb
 from forms.PUESTO.puesto import Puesto
 import random
+from werkzeug.security import generate_password_hash, check_password_hash
 
 DB = mongodb.dbConecction()
 
 def PuestoDb():
-    titulo="Base de datos puesto"
-    puestos= DB ['puestos']
-    puestosRecibidos=puestos.find()
-    return render_template('ADMINISTRADOR/puestos/base_datos.html',titulo=titulo,puesto=puestosRecibidos)
-
+    if 'usuario-administrador' in session:
+        titulo="Base de datos puesto"
+        puestos= DB ['puestos']
+        puestosRecibidos=puestos.find()
+        return render_template('ADMINISTRADOR/puestos/base_datos.html',titulo=titulo,puesto=puestosRecibidos)
+    elif 'usuario-empleado' in session:
+        return redirect('/INICIAR-SESION-EMPLEADO')
 def Formulario():
         titulo="Nuevo puesto"
         OperativosDB=DB['operativos']
@@ -18,32 +21,43 @@ def Formulario():
         return render_template('ADMINISTRADOR/puestos/registrar.html', titulo=titulo,op=OperativosRecibidos)
        
 def Nuevo():
-    nombre=request.form['nombre']
-    correo=request.form['correo']
-    edad=request.form['edad']
-    telefono=request.form['telefono']
-    tipo_puesto=request.form['tipo_puesto']
-    password=request.form['password']
-    activo = False
-    identificador=str(random.randint(0,2000))+correo
+    if 'usuario-administrador' in session:
+        nombre=request.form['nombre']
+        correo=request.form['correo']
+        edad=request.form['edad']
+        telefono=request.form['telefono']
+        tipo_puesto=request.form['tipo_puesto']
+        password=request.form['password']
+        activo = False
+        identificador=str(random.randint(0,2000))+correo
    
-    if nombre and correo and edad and telefono and tipo_puesto and password:
+        if nombre and correo and edad and telefono and tipo_puesto and password:
+            key = generate_password_hash(password, method = 'sha256')
             puestos=DB['puestos']
-            puesto=Puesto(identificador,nombre,correo,edad,telefono,tipo_puesto,password,activo) 
+            puesto=Puesto(identificador,nombre,correo,edad,telefono,tipo_puesto,key,activo) 
             puestos.insert_one(puesto.datoPuestoJson())
             return redirect('/REGISTRAR-PUESTO')  
+    
+    elif 'usuario-empleado' in session:
+        return redirect('/INICIAR-SESION-EMPLEADO') 
 
 def Operaciones_Puesto():  
-    titulo="Operaciones puesto"
-    puestos= DB ['puestos']
-    puestosRecibidos=puestos.find()
-    return render_template('ADMINISTRADOR/puestos/operaciones.html',titulo=titulo,puesto=puestosRecibidos)
+    if 'usuario-administrador' in session:
+        titulo="Operaciones puesto"
+        puestos= DB ['puestos']
+        puestosRecibidos=puestos.find()
+        return render_template('ADMINISTRADOR/puestos/operaciones.html',titulo=titulo,puesto=puestosRecibidos)
+
+    elif 'usuario-empleado' in session:
+        return redirect('/INICIAR-SESION-EMPLEADO')
 
 def Informacion_Puesto(key):
         titulo="Informacion puesto"
-        puestos= DB['puestos']
+        puestos = DB['puestos']
+        puestoOperativo = DB['operativos']
+        operativo = puestoOperativo.find()    
         puestoRecibido=puestos.find_one({'identificador':key})
-        return render_template('ADMINISTRADOR/puestos/informacion.html',titulo=titulo, puestos=puestoRecibido) 
+        return render_template('ADMINISTRADOR/puestos/informacion.html',titulo=titulo, puestos=puestoRecibido,operativo=operativo) 
 
 def Eliminar_Puesto(key):
     puestos= DB['puestos']
